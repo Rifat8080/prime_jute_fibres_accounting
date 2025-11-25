ea8f802171d95c2fbd90b590ab305fbda32478c1
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-11-25 15:11:23 +0600

    flixed flowbite

---
+++ app/javascript/application.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "flowbite"
+
+document.addEventListener("turbo:load", () => {
+    // Initialize Flowbite components
+    initFlowbite();
+});
---
+++ config/importmap.rb
@@ -0,0 +1,6 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "flowbite" # @4.0.0
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "flowbite-datepicker" # @1.3.2
