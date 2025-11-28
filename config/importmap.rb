c221a04b63cd62cb610c4e2b743cd26a82117d5c
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-11-28 17:37:02 +0600

    fixed proper js

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,5 @@
 
 pin "application"
 pin "flowbite" # @4.0.0
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/dist/esm/popper.js"
 pin "flowbite-datepicker" # @1.3.2
